219e881b487851a9817dd262b6708fe57a92f132
Katarine1 <kba.2879@gmail.com>
2019-12-29 10:05:39 -0300

    Auto School App in C# on Console

---
+++ App3_DrivingSchool/Model/School.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3_DrivingSchool.Model
+{
+    public class School
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public School() { }
+
+        public School(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string CheckAge(int age)
+        {
+            if(age < 18)
+            {
+                return "Without permission.";
+            }
+            else if (age >= 18)
+            {
+                return "Permission granted.";
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Age: " + Age;
+        }
+    }
+}
---
+++ App3_DrivingSchool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using App3_DrivingSchool.Model;
+
+namespace App3_DrivingSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("\t\t# -- Driving Scholl -- #");
+
+            Console.WriteLine();
+            Console.Write("\t\tEnter your Name: ");
+            string name = Console.ReadLine();
+            Console.Write("\t\tEnter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            School school = new School(name, age);
+            string a = school.CheckAge(age);
+
+            Console.WriteLine("\t\t" + school + ", " + a + "\n\n");
+        }
+    }
+}
